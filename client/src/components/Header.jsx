9ae2f84fecd9b4b4407421e98a6a1207f0cf3240
od-vivek <odedravivek.10@gmail.com>
2023-11-07 09:08:38 +0530

    added logo to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaSearch } from 'react-icons/fa';
+import logo from '../images/logo.png';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -8,8 +9,12 @@ export default function Header() {
   return (
     <header className='bg-color1'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-color4'></h1>
+        <Link className='flex items-center gap-3' to='/'>
+          <img
+            alt='logo'
+            src={logo}
+            className='max-h-10' // Set a maximum height for the logo
+          />
           <span className='font-bold text color4'>GlobeTrotter</span>
         </Link>
         <form className='text-center bg-color1 p-2 rounded-full flex justify-center items-center hover:scale-105 hover:px-5 border-2 border-color3'>
@@ -28,7 +33,7 @@ export default function Header() {
             <li className='text-color4 hover:text-color3 hover:scale-110'>Home</li>
           </Link>
           <Link to='/about'>
-            <li className='text-color4 hover:text-color3 hover:scale-110'>About Us </li>
+            <li className='text-color4 hover:text-color3 hover:scale-110'>About Us</li>
           </Link>
           <Link to='/login'>
             <li className='text-color4 hover:text-color3 hover:scale-110'>Login</li>
@@ -36,5 +41,5 @@ export default function Header() {
         </ul>
       </div>
     </header>
-  )
+  );
 }
